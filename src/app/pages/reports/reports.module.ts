348fa69d75cd4ae17944754fe827522b2a033984
Robin Cyrolies <robincyrolies@yahoo.com>
2018-12-04 08:30:32 +0200

    Master Committed

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { ReportsPage } from './reports';
+//import { PopoverPage } from '../about-popover/about-popover';
+import { ReportsPageRoutingModule } from './reports-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReportsPageRoutingModule
+  ],
+  declarations: [ReportsPage],
+ // entryComponents: [PopoverPage],
+  bootstrap: [ReportsPage],
+})
+export class ReportsModule {}

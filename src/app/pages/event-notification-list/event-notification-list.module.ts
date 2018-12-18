e9099a5d3ed70116b7ebcbb572f5b840e4a59fa2
Werner Stoltsz <45585872+wernerstoltsz@users.noreply.github.com>
2018-12-18 17:00:57 +0200

    Cards for Event Notifications page.

--- src/app/pages/event-notification-list/event-notification-list.module.ts
+++ src/app/pages/event-notification-list/event-notification-list.module.ts
@@ -4,11 +4,13 @@ import { IonicModule } from '@ionic/angular';
 
 import { EventNotificationListPage } from './event-notification-list';
 import { EventNotificationListPageRoutingModule } from './event-notification-list-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    FormsModule,
     EventNotificationListPageRoutingModule
   ],
   declarations: [EventNotificationListPage],
--- src/app/pages/event-notification-list/event-notification-list.ts
+++ src/app/pages/event-notification-list/event-notification-list.ts
@@ -1,9 +1,9 @@
+import { FreightApiService } from './../../providers/freight-api.service';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ActionSheetController, NavController } from '@ionic/angular';
-
-import { ConferenceData } from '../../providers/conference-data';
+import { ShipmentEvent } from '../../providers/freight-api.service';
+import { UserData } from '../../providers/user-data';
 
 @Component({
   selector: 'page-event-notification-list',
@@ -12,95 +12,33 @@ import { ConferenceData } from '../../providers/conference-data';
   encapsulation: ViewEncapsulation.None
 })
 export class EventNotificationListPage {
-  speakers: any[] = [];
+  notifications: ShipmentEvent[] = [];
 
   constructor(
     public actionSheetCtrl: ActionSheetController,
-    public confData: ConferenceData,
-    public inAppBrowser: InAppBrowser,
+    public freightService: FreightApiService,
+    public userData: UserData,
     public router: Router,
     public navCtrl: NavController
   ) {}
 
   ionViewDidEnter() {
-    this.confData.getSpeakers().subscribe((speakers: any[]) => {
-      this.speakers = speakers;
+    // Retrieve x-months worth of notifications, according to spec in server config.
+    // this.freightService.GetShipmentEvents(this.userData.selectedProfile.CargoWiseCode)
+    this.freightService.GetShipmentEvents('SIMFISSEA') // TODO: Remove hardcoded cargowise code.
+    .subscribe((result: ShipmentEvent[]) => {
+      this.notifications = result;
+      this.notifications.sort(this.eventDateComparer).reverse();
     });
   }
 
-  goToSessionDetail(session: any) {
-    this.router.navigateByUrl(`app/tabs/(speakers:session/${session.id})`);
+  eventDateComparer(a: ShipmentEvent, b: ShipmentEvent) {
+    if (a.CreateDateTime < b.CreateDateTime){
+      return -1;
+    } else if (a.CreateDateTime > b.CreateDateTime) {
+      return 1;
+    }
+    return 0;
   }
 
-  goToSpeakerDetail(speaker: any) {
-    this.router.navigateByUrl(
-      `app/tabs/(speakers:speaker-details/${speaker.id})`
-    );
-  }
-
-  goToSpeakerTwitter(speaker: any) {
-    this.inAppBrowser.create(
-      `https://twitter.com/${speaker.twitter}`,
-      '_blank'
-    );
-  }
-
-  async openSpeakerShare(speaker: any) {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Share ' + speaker.name,
-      buttons: [
-        {
-          text: 'Copy Link',
-          handler: () => {
-            console.log(
-              'Copy link clicked on https://twitter.com/' + speaker.twitter
-            );
-            if (
-              (window as any)['cordova'] &&
-              (window as any)['cordova'].plugins.clipboard
-            ) {
-              (window as any)['cordova'].plugins.clipboard.copy(
-                'https://twitter.com/' + speaker.twitter
-              );
-            }
-          }
-        },
-        {
-          text: 'Share via ...'
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-
-    await actionSheet.present();
-  }
-
-  async openContact(speaker: any) {
-    const mode = 'ios'; // this.config.get('mode');
-
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Contact ' + speaker.name,
-      buttons: [
-        {
-          text: `Email ( ${speaker.email} )`,
-          icon: mode !== 'ios' ? 'mail' : null,
-          handler: () => {
-            window.open('mailto:' + speaker.email);
-          }
-        },
-        {
-          text: `Call ( ${speaker.phone} )`,
-          icon: mode !== 'ios' ? 'call' : null,
-          handler: () => {
-            window.open('tel:' + speaker.phone);
-          }
-        }
-      ]
-    });
-
-    await actionSheet.present();
-  }
 }

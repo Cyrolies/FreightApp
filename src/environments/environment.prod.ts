d50676db4301d9040b28783db86710ff7017ebec
Werner Stoltsz <45585872+wernerstoltsz@users.noreply.github.com>
2019-01-10 15:38:19 +0200

    Add auth token to production env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   defaultUser: undefined,
   defaultPassword: undefined,
   useOAuth: true,
-  defaultOAuthToken: undefined
+  // tslint:disable-next-line:max-line-length
+  defaultOAuthToken: 'hxNdTyKZFDHnGebt52Owl7tQuw8tTk4-RK1xnOc0P08ZU4l9d1_brCL2O8y53dzOwRvS5YP9F4jeNkvOLMbncgfgMnE7oyY5_B_J9irnJN8PpGSk-rk1bb3WxD4QnXVeqDKbg7va39GxOrxCRH-rqxD9LRXwN2lq1D8HJCDLOerysSZNgtzBcRG_7gt7KNgKBFR4cfBN9cclEsVthMjQIhvUHru9GTwQRL92b9tIYjEsdEUa6CXOF_QwnWobw6sSoxHg3RmvY4DM2Duy1TWUKfTM7ioMlw8S2AKRO7Dt4AnViTHCftw4-hewUp2YYj0k0IXNWhECAg1KxLKpMlQZdpjW3YP-P2BZ4QQX9wucqqt9Vq_-HP4i69Fc9YoeSpf13mPSFYcDdXtolBByzpo2iRO5RNa9dYQIZO22NBtTCjcCFwER1wAOLNPKRVLVvPRvaBjkMMNeoRz6L-otGUUyI9SnXB2EHZahwGstIvM3hAXxF8Eta7FhRU1Ypftwnwapmi6kHwlQNDql7-UQiiUzwANncSV84ZSHckOOGCRnsbw'
 };
